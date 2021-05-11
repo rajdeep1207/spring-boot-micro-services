2112f01a913aa866d68d3a2122be5ebe0d8b5605
Rajdeep <rajdeeppilla@gmail.com>
2021-05-11 23:24:51 +0530

    first commit

---
+++ currency-conversion-service/src/main/java/com/rajdeep/microservices/currencyconversionservice/CurrencyConversion.java
@@ -0,0 +1,22 @@
+package com.rajdeep.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConversion {
+	private Long id;
+	private String from;
+	private String to;
+	private BigDecimal quantity;
+	private BigDecimal conversionMultiple;
+	private BigDecimal totalCalculatedValue;
+	private String environment;
+}
---
+++ limits-service/src/main/java/com/rajdeep/microservices/limitsservice/configuration/Configuration.java
@@ -0,0 +1,16 @@
+package com.rajdeep.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@ConfigurationProperties("limits-service")
+@Getter
+@Setter
+@Component
+public class Configuration {
+	private int minimum;
+	private int maximum;
+}
